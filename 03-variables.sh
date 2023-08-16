bd9e65411ddc0058aefcb7289d1665c6add532c5
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-16 19:30:24 +0530

    Ramesh and Suresh conversation

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hello world, I am learning DevOps with AWS"
+
+PERSON1=$1 # This is the first value from command line
+PERSON2=$2 # This is the second value from command line
+
+
+echo "$PERSON1: Hi $PERSON2, Good morning"
+echo "$PERSON2: Good morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, how are you $PERSON1"
