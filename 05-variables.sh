ea536ab431c82f19b0baadd25b401d557b9e5969
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-16 21:29:56 +0530

    prompting the user name and password

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter your user name: "
+
+read USERNAME # This is the variable where the value of the one user entered
+
+echo "Please enter your password: "
+read -s PASSWORD # This is the variable where the password is entered
+
+echo "User name entered is $USERNAME"
+echo "Password enterd is $PASSWORD"
+
