f0a9230050c50b272a0f05e686eaf377ad453f84
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-16 18:16:18 +0530

    Ramesh and Suresh conversation

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "Hello world, I am learning DevOps with AWS"
-echo "Ramesh: Hi Suresh, Good morning"
-echo "Suresh: Good morning Ramesh"
-echo "Ramesh: How are you Suresh"
-echo "Suresh: I am good, how are you Ramesh"
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+
+echo "$PERSON1: Hi $PERSON2, Good morning"
+echo "$PERSON2: Good morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, how are you $PERSON1"
