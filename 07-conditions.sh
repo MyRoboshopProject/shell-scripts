6f3e39eed4c5a1539dd935748475ed90173ca74f
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-17 11:04:31 +0530

    If condition checks

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,9 +2,9 @@
 
 # check if your number is greate than 10 or not
 NUMBER=$1
-if [NUMBER -ne 10]
+if [NUMBER -gt 10]
 then
-    echo "The number is not equalent to 10"
+    echo "The number is greater than 10"
 else
-    echo "The number is equalent to 10"
+    echo "The number is not greater than 10"
 fi
