ebb84bc6d775a1033597331359146fa1504f7a73
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-17 11:30:28 +0530

    Install mysql

---
+++ 08-install-mysql.sh
@@ -0,0 +1,15 @@
+
+# Install My SQL
+
+#!/bin/bash
+
+USERID=$(id -u)
+
+if $USERID -ne 0
+then
+    echo "ERROR:: Please run with sudo user"
+else
+    echo "you are the root user"
+fi
+
+yum install mysql -y
