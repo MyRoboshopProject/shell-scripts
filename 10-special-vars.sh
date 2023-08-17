e406d677cda2314420ffc920f627669d9d3995e2
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-17 17:15:23 +0530

    special variables

--- 10-special-vars.sh
+++ 10-special-vars.sh
@@ -3,4 +3,11 @@
 VAR1=$1
 VAR2=$2
 
+# print only the first input provided by user
 echo "variable 1: $VAR1"
+
+# print all the iputs provided by user
+echo "All variables: $@"
+
+# print how many numbers provided by user
+echo "No. of vars: $#"
