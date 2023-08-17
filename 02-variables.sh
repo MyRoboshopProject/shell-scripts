9843570f96834cce69c2467a4d6d11437de4c998
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-17 10:03:49 +0530

    formated the date

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,7 +2,7 @@
 
 # Date variable
 # shell script run the command inside $() and stores the output into variable called DATE
-DATE=$(date+%F)
+DATE=$(date +%F)
 
 
 echo "Hello this script is executed at timestamp: $DATE"
