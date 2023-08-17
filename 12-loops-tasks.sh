f394b34e19ff106e68c32b5d0264681e07d1ccd1
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-17 21:20:11 +0530

    loops-tasks

--- 12-loops-tasks.sh
+++ 12-loops-tasks.sh
@@ -8,8 +8,9 @@ DATE=$(date +%F)
 
 # should run with sudo user
 if [ $USERID -ne 0 ]
-echo "ERROR:: Please run with sudo user"
-exit 1
+then
+    echo "ERROR:: Please run with sudo user"
+    exit 1
 fi
 
 # for Loop to install the packages with user inputs 
