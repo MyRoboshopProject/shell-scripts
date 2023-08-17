ede82dc44f3ab06d1e3202482793775e8c502aea
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-17 11:50:47 +0530

    Install mysql

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -9,9 +9,8 @@ if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run with sudo user"
     exit 1
-
-else
-    echo "you are the root user"
+# else
+#     echo "you are the root user"
 fi
 
 yum install mysql -y
