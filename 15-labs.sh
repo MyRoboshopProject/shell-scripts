62ce6bad139869f7e6d5a03bbe9fca981c4f4ba8
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-18 22:48:09 +0530

    multiple packages installations

--- 15-labs.sh
+++ 15-labs.sh
@@ -3,9 +3,11 @@
 # read the lines from the input file using while loop method
 
 INPUTFILE=$1
+DATE=$(date +%F)
+LOGFILE=/home/centos/$DATE-reverse.log
 
 while read line
 do
-    rev $INPUTFILE
+    rev $INPUTFILE &>>$LOGFILE
 
 done < $INPUTFILE
