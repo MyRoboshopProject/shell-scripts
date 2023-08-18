5092faacaaf597037e56cdcfd10fdaf1a8c816ed
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-18 11:42:02 +0530

    multiple packages installations

--- 12-loops-pkg-installs.sh
+++ 12-loops-pkg-installs.sh
@@ -2,9 +2,9 @@
 
 DATE=$(date +%F)
 USERID=$(id -u)
-LOGDIR=/home/centos/shellscript-logs
+LOGSDIR=/home/centos/shellscript-logs
 SCRIPT_NAME=$0
-LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE
 N="\e[0m"
 R="\e[31m"
 G="\e[32m"
@@ -13,6 +13,29 @@ Y="\e[33m"
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERROR:: $N Please run the script with the root user"
+    exit 1
 fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "Installing $2... $R FAILURE $N"
+    else
+        echo -e "Installing $2... $G SUCCESS $N"
+    fi
+}
+
+for i in $@
+do
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "The package is not installed, let's installe it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? $i       
+    else
+        echo "$Y $i is already installed"
+    fi    
+done
 
 
