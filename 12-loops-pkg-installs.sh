519090d361d04eed9358e6479a425664279cb09e
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-18 11:08:15 +0530

    Package installations script

--- 12-loops-pkg-installs.sh
+++ 12-loops-pkg-installs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-DATE=$(date +%F)
 USERID=$(id -u)
-LOGSDIR=/home/centos/shell-script-logs
-LOGFILE=$LOG-DIR-$DATE.log
+
 N="\e[0m"
 R="\e[31m"
 G="\e[32m"
@@ -11,6 +9,7 @@ Y="\e[33m"
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: $N Please run the script with root access"
-    exit 1
+    echo "$R ERROR:: $N Please run the script with the root user"
 fi
+
+
