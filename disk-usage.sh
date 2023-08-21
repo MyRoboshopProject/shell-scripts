c3a300d6c50ea04404a6a843333256faa3b0d674
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-21 15:59:37 +0530

    disk utilization notifications

--- disk-usage.sh
+++ disk-usage.sh
@@ -6,19 +6,21 @@
 # Validations
 # logs
 
+LOGFILE_DIRECTORY=/tmp
 DATE=$(date +%F)
-USERID=$(id -u)
 SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+
 N="\e[0m"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
-DISK_USAGE=$(df -hT | grep -vE "tmpfs|Filesystem" | awk '{print $6}' | cut -d% -f1)
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
 message=""
 
+#IFS= means internal feild seperator is space.
 while IFS= read line
 do
     # this command will give you usage in number format for comparision
@@ -26,7 +28,7 @@ do
     # this command will give us partition
     partition=$(echo $line | awk {print $1}")
     #now you need to check whether it is more than threshold or not
-    if [ $usage -ge $DISK_USAGE_THRESHOLD ]
+    if [ $usage -gt $DISK_USAGE_THRESHOLD ];
     then
         message+="HIGH DISK USAGE on $partition: $usage\n"
     fi
