c5080d811a19d387f8a88a0fb9a87fadd59b8424
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-21 16:01:34 +0530

    disk utilization notifications

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Continously check disk memory and send alert emails if it is more then threshold
-
-# Colors
-# Validations
-# logs
+# colors
+# validations
+# log redirections
 
 LOGFILE_DIRECTORY=/tmp
-DATE=$(date +%F)
+DATE=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
 LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
 
-N="\e[0m"
 R="\e[31m"
 G="\e[32m"
+N="\e[0m"
 Y="\e[33m"
 
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
@@ -22,11 +20,11 @@ message=""
 
 #IFS= means internal feild seperator is space.
 while IFS= read line
-do
+do  
     # this command will give you usage in number format for comparision
-    usage=$(echo $line | awk '{print $6}' | cut -d% -f1)
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
     # this command will give us partition
-    partition=$(echo $line | awk {print $1}")
+    partition=$(echo $line | awk '{print $1}')
     #now you need to check whether it is more than threshold or not
     if [ $usage -gt $DISK_USAGE_THRESHOLD ];
     then
@@ -34,4 +32,9 @@ do
     fi
 done <<< $DISK_USAGE
 
-# echo -e "message: $message"
+echo -e "message: $message"
+
+#echo "$message" | mail -s "High Disk usage" info@joindevops.com
+
+#how to call other shell script from your current script
+# sh mail.sh info@joindevops.com "High Disk Usage" "$message" "DEVOPS TEAM" "High Disk usage"
