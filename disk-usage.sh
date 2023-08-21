e544f123d9d476e42579c0281673901039d653e5
Raghunaga <raghunaga.yara@vistracorp.com>
2023-08-21 15:54:36 +0530

    disk utilization notifications

--- disk-usage.sh
+++ disk-usage.sh
@@ -21,14 +21,15 @@ message=""
 
 while IFS= read line
 do
-# this command will give you usage in number format for comparision
-usage=$(echo $line | awk '{print $6}' | cut -d% -f1)
-# this command will give us partition
-partition=$(echo $line | awk {print $1}")
-if [ $usage -ge $DISK_USAGE_THRESHOLD ]; then
-    message+="HIGH DISK USAGE on $partition: $usage\n"
-fi
-
-done >>> $DISK_USAGE
+    # this command will give you usage in number format for comparision
+    usage=$(echo $line | awk '{print $6}' | cut -d% -f1)
+    # this command will give us partition
+    partition=$(echo $line | awk {print $1}")
+    #now you need to check whether it is more than threshold or not
+    if [ $usage -ge $DISK_USAGE_THRESHOLD ]
+    then
+        message+="HIGH DISK USAGE on $partition: $usage\n"
+    fi
+done <<< $DISK_USAGE
 
 echo -e "message: $message"
